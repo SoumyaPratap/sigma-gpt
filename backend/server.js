62c5419d636654f86c82a1d276be5d4724ba1ab4
SOUMYA PRATAP SINGH <soumyapratapsingh0001@gmail.com>
2026-06-21 11:05:18 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,7 @@ const app = express();
 connectDB();
 
 // Middlewares
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use(express.json());
 
