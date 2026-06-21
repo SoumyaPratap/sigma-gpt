7606fa02b96f92d292f62e5609de871bac4cea6a
SOUMYA PRATAP SINGH <soumyapratapsingh0001@gmail.com>
2026-06-21 11:01:31 +0530

    Fix signup API

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -21,7 +21,7 @@ function Signup() {
 
             const response =
                 await axios.post(
-+                   "https://sigma-gpt-backend-oh77.onrender.com/api/auth/signup",
+                  "https://sigma-gpt-backend-oh77.onrender.com/api/auth/signup",
                     {
                         name,
                         email,
